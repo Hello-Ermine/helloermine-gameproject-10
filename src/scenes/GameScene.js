0d6ae19b500305254e5fd893ec1e1cad04c636ff
Chanpol Thippawan <chanpol.thip@kmutt.ac.th>
2021-11-17 16:35:13 +0700

    obj

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -8,8 +8,15 @@ let keySb;
 let jump;
 
 let wood;
-let woodevent;
+let wood2;
+let woodEvent;
 let woodGroup;
+
+let shuriken;
+let shuriken2;
+let shurikenEvent;
+let shurikenGroup;
+
 
 
 class GameScene extends Phaser.Scene {
@@ -26,6 +33,9 @@ class GameScene extends Phaser.Scene {
         this.load.image('ground','src/image/layers/ground.png');
         this.load.spritesheet('ninja','src/image/spritesheet (7).png',{frameWidth: 218, frameHeight : 164});
         this.load.image('wood','src/image/obj/wood.png');
+        this.load.image('wood2','src/image/obj/wood2.png');
+        this.load.image('shuriken','src/image/obj/shuriken.png');
+        this.load.image('shuriken2','src/image/obj/shuriken2.png');
 
     
     }
@@ -37,19 +47,36 @@ class GameScene extends Phaser.Scene {
         
 
         woodGroup = this.physics.add.group();
+        shurikenGroup = this.physics.add.group();
 
-        woodevent = this.time.addEvent({
-            delay: 5000,
+        woodEvent = this.time.addEvent({
+            delay: 7000,
             callback: function () {
-                wood = this.physics.add.image(1500, 435, 'wood').setScale(0.5).setDepth(3).setSize(135,380).setOffset(190,120);
+                wood = this.physics.add.image(Phaser.Math.Between(1500,2000), 435, 'wood').setScale(0.5).setDepth(3).setSize(135,380).setOffset(190,120);
+                wood2 = this.physics.add.image(Phaser.Math.Between(2000,3000), 435, 'wood2').setScale(0.5).setDepth(3).setSize(135,380).setOffset(190,120);
                 woodGroup.add(wood);
-                woodGroup.setVelocityX(-400);
+                woodGroup.add(wood2);
+                woodGroup.setVelocityX(-500);
                 this.physics.add.overlap(woodGroup, ninja);
             },
             callbackScope: this,
             loop: true,
         });
     
+        shurikenEvent = this.time.addEvent({
+            delay: 7000,
+            callback: function () {
+                shuriken = this.physics.add.image(Phaser.Math.Between(1500,5000), 515, 'shuriken').setScale(0.5).setDepth(3).setSize(250,220).setOffset(140,120);
+                shuriken2 = this.physics.add.image(Phaser.Math.Between(3000,4000), 495, 'shuriken2').setScale(0.5).setDepth(3).setSize(220,220).setOffset(110,160);
+                shurikenGroup.add(shuriken);
+                shurikenGroup.add(shuriken2);
+                shurikenGroup.setVelocityX(-500);
+                this.physics.add.overlap(shurikenGroup, ninja);
+            },
+            callbackScope: this,
+            loop: true,
+        });
+        
        
         this.anims.create({
             key: 'ninjaRun',
@@ -102,21 +129,12 @@ class GameScene extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(keySb) && (ninja.body.touching.down || canDoubleJump )) {
             jump.play({loop: false});
             ninja.jumpCount++;
-            ninja.setVelocityY(-900);
+            ninja.setVelocityY(-960);
             ninja.anims.play('ninjaJump', true,)
         }
-            // if (keySb.isDown && Phaser.Input.Keyboard.JustDown(keySb)) {
-        //     jump.play({loop: false});
-        //     // ninja.anims.play('ninjaJump', true,)
-        //     ninja.anims.play('ninjaJump', true,)
-        //     ninja.setVelocityY(-500);
-            
-        // }
         
         else if (keySb.isDown) {
-            // setTimeout(function() { 
-            //     ninja.anims.play('ninjaJump', true,)
-            //     ninja.setVelocityY(-100) }, 1000);
+
         }
         else{ 
         ninja.anims.play('ninjaRun', true);
@@ -127,7 +145,7 @@ class GameScene extends Phaser.Scene {
                     woodGroup.getChildren()[i].destroy();
             }
         }
-        bg.tilePositionX += 4;
+        bg.tilePositionX += 6;
     } 
 }
 
